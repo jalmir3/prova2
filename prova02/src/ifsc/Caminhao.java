bc0309fa5b7b877243f3e2bff66baeea7ed81f79
Jalmir Winter <104005909+jalmir3@users.noreply.github.com>
2022-10-17 20:25:05 -0300

    Update Caminhao.java

--- prova02/src/ifsc/Caminhao.java
+++ prova02/src/ifsc/Caminhao.java
@@ -5,8 +5,7 @@ public class Caminhao extends Veiculo{
 	private int eixos;
        public Caminhao() {}
        public Caminhao(String placa,int ano,int eixos) {
-	      super.getPlaca();
-	      super.getAno();
+	      super(placa,ano);
 	      this.eixos =eixos;	
 	}
 	public int getEixos() {
